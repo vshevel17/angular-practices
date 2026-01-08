1e0558cab1e4ea9844e9e2097f28d34b1081d3dd
vshevel17 <v.shevel@student.sumdu.edu.ua>
2026-01-08 22:27:12 +0200

    Add quiz and question components

---
+++ pr-04/src/app/components/question/question.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-question',
+  imports: [],
+  templateUrl: './question.html',
+  styleUrl: './question.css',
+})
+export class Question {
+
+}
